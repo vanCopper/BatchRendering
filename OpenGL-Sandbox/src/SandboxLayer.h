9e5fb2d48ba1d5d45ed2d62b3ad893f59c361c4f
vanCopper <vancopper@foxmail.com>
2021-04-25 17:18:22 +0800

    Batch Rendering

---
+++ OpenGL-Sandbox/src/SandboxLayer.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <GLCore.h>
+#include <GLCoreUtils.h>
+#include "Renderer.h"
+
+class SandboxLayer : public GLCore::Layer
+{
+public:
+	SandboxLayer();
+	virtual ~SandboxLayer();
+
+	virtual void OnAttach() override;
+	virtual void OnDetach() override;
+	virtual void OnEvent(GLCore::Event& event) override;
+	virtual void OnUpdate(GLCore::Timestep ts) override;
+	virtual void OnImGuiRender() override;
+private:
+	//std::unique_ptr<Shader> m_Shader;
+	GLCore::Utils::Shader* m_Shader;
+	GLuint m_CTex;
+	GLuint m_GlTex;
+	GLCore::Utils::OrthographicCameraController m_CameraController;
+	const glm::vec2& m_QuadPosition = { 1.0f, 1.0f };
+};
